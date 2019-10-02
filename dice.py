718a7789f133e01c3c87805c197edd8617641533
TranD2020 <56050476+TranD2020@users.noreply.github.com>
2019-10-02 15:03:25 -0700

    Added Stuff In

--- dice.py
+++ dice.py
@@ -1,3 +1,13 @@
-# Name:
-# Period
+# Name: Dennis Tran
+# Period 6
 # Dice Rolling Simulator
+import random
+random.randint(1,6)
+
+print("Welcome to the Dice Game. ")
+print("Input the number of times a 6 sided die will be rolled: ")
+rolls = int(input("how many rolls: "))
+x = 1
+
+while x <= rolls: 
+	x = x + 1
