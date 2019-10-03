448033cef7342e1b7c9b0ef8a9086f962c831a0f
TranD2020 <56050476+TranD2020@users.noreply.github.com>
2019-10-03 15:02:57 -0700

    Generate list and random numbers

--- dice.py
+++ dice.py
@@ -3,11 +3,29 @@
 # Dice Rolling Simulator
 import random
 random.randint(1,6)
-
+# Welcome to Dice Game
 print("Welcome to the Dice Game. ")
+
+# Result Score
+def printResults():
+	print("1s - ")
+	print("2s - ")
+	print("3s - ")
+	print("4s - ")
+	print("5s - ")
+	print("6s - ")
+
 print("Input the number of times a 6 sided die will be rolled: ")
-rolls = int(input("how many rolls: "))
+numRolls = int(input("how many rolls: "))
 x = 1
 
-while x <= rolls: 
-	x = x + 1
+while x <= numRolls: 
+	x += 1
+	x <= 100
+	print(random.randint(1,6))
+	
+	# Show Results
+	printResults()
+while True: 
+	if x == 1:
+		pass
