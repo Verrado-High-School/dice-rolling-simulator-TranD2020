00a0042c37c06f7618cdcb2989ec6fe6819f598e
TranD2020 <56050476+TranD2020@users.noreply.github.com>
2019-10-04 15:00:26 -0700

    Generate Percentages and Count

--- dice.py
+++ dice.py
@@ -7,13 +7,29 @@ random.randint(1,6)
 print("Welcome to the Dice Game. ")
 
 # Result Score
-def printResults():
-	print("1s - ")
-	print("2s - ")
-	print("3s - ")
-	print("4s - ")
-	print("5s - ")
-	print("6s - ")
+
+numOne = 0
+numTwo = 0
+numThree = 0
+numFour = 0
+numFive = 0
+numSix = 0
+
+def printNum():
+	print("1s - " + str(numOne))
+	print("2s - " + str(numTwo))
+	print("3s - " + str(numThree))
+	print("4s - " + str(numFour))
+	print("5s - " + str(numFive))
+	print("6s - " + str(numSix))
+
+def printPercent():
+	print("1s - " + str(numOne / 100) + "%")
+	print("2s - " + str(numTwo / 100) + "%")
+	print("3s - " + str(numThree / 100) + "%")
+	print("4s - " + str(numFour / 100) + "%")
+	print("5s - " + str(numFive / 100) + "%")
+	print("6s - " + str(numSix / 100) + "%")
 
 print("Input the number of times a 6 sided die will be rolled: ")
 numRolls = int(input("how many rolls: "))
@@ -21,11 +37,22 @@ x = 1
 
 while x <= numRolls: 
 	x += 1
-	x <= 100
-	print(random.randint(1,6))
+	RandNum = random.randint(1,6)
+	print(RandNum)
+	if RandNum == 1:
+		numOne += 1
+	elif RandNum == 2:
+		numTwo += 1
+	elif RandNum == 3:
+		numThree += 1
+	elif RandNum == 4:
+		numFour += 1
+	elif RandNum == 5:
+		numFive += 1
+	elif RandNum == 6:
+		numSix += 1
+		
+printNum()
+printPercent()
 	
-	# Show Results
-	printResults()
-while True: 
-	if x == 1:
-		pass
+	
