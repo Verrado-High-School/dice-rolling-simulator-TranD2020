99a5524688acb31847cfb3d8c44e7f6745882a75
TranD2020 <56050476+TranD2020@users.noreply.github.com>
2019-10-15 14:17:26 -0700

    Revise Percentages

--- dice.py
+++ dice.py
@@ -7,14 +7,12 @@ random.randint(1,6)
 print("Welcome to the Dice Game. ")
 
 # Result Score
-
 numOne = 0
 numTwo = 0
 numThree = 0
 numFour = 0
 numFive = 0
 numSix = 0
-
 
 def printNum():
 	print("1s - " + str(numOne))
@@ -24,12 +22,12 @@ def printNum():
 	print("5s - " + str(numFive))
 	print("6s - " + str(numSix))
 def printPercent():
-	print("1s - " + str(numOne / 100) + "%")
-	print("2s - " + str(numTwo / 100) + "%")
-	print("3s - " + str(numThree / 100) + "%")
-	print("4s - " + str(numFour / 100) + "%")
-	print("5s - " + str(numFive / 100) + "%")
-	print("6s - " + str(numSix / 100) + "%")
+	print("1s - " + str(numOne / 100 * 100) + "%")
+	print("2s - " + str(numTwo / 100 * 100) + "%")
+	print("3s - " + str(numThree / 100 * 100) + "%")
+	print("4s - " + str(numFour / 100 * 100) + "%")
+	print("5s - " + str(numFive / 100 * 100) + "%")
+	print("6s - " + str(numSix / 100 * 100) + "%")
 
 print("Input the number of times a 6 sided die will be rolled: ")
 numRolls = int(input("how many rolls: "))
